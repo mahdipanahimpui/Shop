21981c4aab159c9beee96d63d46d071dab8d461f
mahdi <mahdipanahimpui@gmail.com>
2023-08-03 11:44:16 +0330

    verify_code

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from . forms import UserRegistrationForm
-from .models import OtpCode
+from . forms import UserRegistrationForm, VerifyCodeForm
+from .models import OtpCode, User
 from django.contrib import messages
 import random
 from utils import send_otp_code
@@ -19,18 +19,21 @@ class UserRegistrationView(View):
         form = self.form_class(requeset.POST)
         if form.is_valid():
             cd = form.cleaned_data
+
+            if User.objects.filter(email=cd['email']).exists() or User.objects.filter(phone_number=cd['phone_number']).exists():
+                messages.error(requeset, 'phone number or email is used before,', 'warning')
+                return render(requeset, self.template_name, {'form': form})
+
             random_code = random.randint(10000, 99999)
-            send_otp_code(
-                phone_number=cd['phone_number'],
-                code=random_code
-            )
+            send_otp_code(phone_number=cd['phone_number'], code=random_code)
+
 
             OtpCode.objects.create(phone_number=cd['phone_number'], code=random_code)
 
             # session is save in server and a hash of that is on client sys
             requeset.session['user_registration_info'] = {
                 'phone_number': cd['phone_number'],
-                'emial': cd['email'],
+                'email': cd['email'],
                 'full_name': cd['full_name'],
                 'password': cd['password']
             }
@@ -42,9 +45,49 @@ class UserRegistrationView(View):
     
 
 class UserRegistrationVerifyCodeView(View):
+    form_class = VerifyCodeForm
+    template_name = 'accounts/verify.html'
 
     def get(self, request):
-        pass
+        # user_session = request.session['user_registration_info']
+        # print(user_session)
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+
+        user_session = request.session['user_registration_info']
+
+        # try:
+        #     code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+        # except OtpCode.DoesNotExist:
+        #     messages.error(request, 'something went wrong, please register again', 'error')
+        #     return redirect('accounts:user_register')
+        
+        code_instances = OtpCode.objects.filter(phone_number=user_session['phone_number'])
+
+        
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+            if any(cd['code'] == code_instance.code for code_instance in code_instances):
+                User.objects.create_user(
+                    phone_number=user_session['phone_number'],
+                    email=user_session['email'],
+                    full_name=user_session['full_name'],
+                    password=user_session['password']
+                )
+                code_instances.delete()
+
+                messages.success(request, 'user registered', 'info')
+                return redirect('home:home')
+            
+            else:
+                messages.error(request, 'code not match', 'danger')
+                return redirect('accounts:verify_code')
+            
+        return redirect('home:home')
+        
+
