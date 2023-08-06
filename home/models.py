1aa21dd28f73a0418872e745093bbf7cfd9b1ba1
mahdi <mahdipanahimpui@gmail.com>
2023-08-06 22:03:18 +0330

    shop_models(category_product_model) _(plural_name)

--- home/models.py
+++ home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category' # the model name used in admin panel
+        verbose_name_plural = 'Categories' # to show in admin pannel (plural name use in admin pannel)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/')
+    # needs to pip install pillow
+    description = models.TextField()
+    # price = models.DecimalField(max_digits=4, decimal_places=2) # 1234.12
+    price = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
