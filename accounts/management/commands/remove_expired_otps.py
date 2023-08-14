a4f5f7b882c2b8c87989bffb385a9ba71fac8e98
mahdi <mahdipanahimpui@gmail.com>
2023-08-14 20:22:43 +0330

    management_commands

---
+++ accounts/management/commands/remove_expired_otps.py
@@ -0,0 +1,31 @@
+# by <python manage.py help> command, can see the statements,
+# HOW add an command to manage.py
+
+# create managemnet directory in app directory
+# create __init__.py in management dir
+# create commands directory in management directory
+# create your own .py file 
+
+
+
+from django.core.management.base import BaseCommand
+from accounts.models import OtpCode
+from datetime import datetime, timedelta
+
+
+# class name should be <Command> 
+class Command(BaseCommand):
+    help = 'remove all expired otp codes'
+
+    # overwride the handle def
+    def handle(self, *args, **kwargs):
+        expired_time = datetime.now() - timedelta(minutes=2)
+        OtpCode.objects.filter(created__lt=expired_time).delete()
+
+        self.stdout.write('all expired otp code removed.')
+
+
+
+
+
+
