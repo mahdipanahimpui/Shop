fddcf7355e844fbb591d3cc7a301cf00682ca50d
mahdi <mahdipanahimpui@gmail.com>
2023-08-15 16:47:32 +0330

    orders_app_blue_print

--- Shop/settings.py
+++ Shop/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
+    'orders.apps.OrdersConfig',
+
+    # Third Party Apps
     'storages',
     'django_celery_beat',
 ]
--- Shop/urls.py
+++ Shop/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
-    path('', include('accounts.urls', namespace='accounts'))
+    path('', include('accounts.urls', namespace='accounts')),
+    path('orders/', include('orders.urls', namespace='orders'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
