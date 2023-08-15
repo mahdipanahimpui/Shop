dea3735a4e2c223e0c3ce158140ce937c2f7d783
mahdi <mahdipanahimpui@gmail.com>
2023-08-15 18:07:51 +0330

    cart_part3_(cart_detail)

--- orders/cart.py
+++ orders/cart.py
@@ -1,4 +1,4 @@
-
+from home.models import Product
 
 CART_SESSION_ID = 'card'
 
@@ -14,14 +14,30 @@ class Cart:
 
     
     def add(self, product, quantity):
-        product_id = product.id
+        product_id = str(product.id)
 
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0, 'price': product.price}
 
         self.cart[product_id]['quantity'] += quantity
-        self.save_session()
+        self.save_session() 
 
 
     def save_session(self):
         self.session.modified = True
+
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids) # using field lookup get all product with product_ids
+
+        temp_cart = self.cart.copy()
+
+        for product in products:
+            temp_cart[str(product.id)]['product_name'] = product.name
+
+        for item in temp_cart.values():
+            item['total_price'] = item['price'] * item['quantity']
+            
+            yield item
+
